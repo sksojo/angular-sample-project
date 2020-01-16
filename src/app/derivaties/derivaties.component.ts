f60518d42d10cf57bd07524edd206bb0efe5121a
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2020-01-16 13:10:03 +0530

    ngSwitchCase to toggle views based on selected tab

--- src/app/derivaties/derivaties.component.ts
+++ src/app/derivaties/derivaties.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DerivatiesComponent implements OnInit {
 
   public courses = [];
-
+  public viewMode = 'map'
   constructor() { }
 
   ngOnInit() {
